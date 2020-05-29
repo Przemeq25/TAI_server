4dfe5c3ab807ccc1d6b8869daf635309e8ad97bf
Przemysław Cichoń <przemekcichon97@gmail.com>
2020-05-29 23:31:54 +0200

    fix

--- app/app.js
+++ app/app.js
@@ -1,45 +1,39 @@
-'use strict';
-
 import express from 'express';
 import morgan from 'morgan';
 import mongoose from 'mongoose';
-import config from './config';
 import routes from './REST/routes';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-const app = express();
+import { config } from './config';
+import path from 'path';
 
+const app = express();
 
 app.use(express.static('public'));
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
+routes(app);
 
-
-mongoose.connect(config.databaseUrl, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true}, (error) => {
+mongoose.connect(config.databaseUrl, { useNewUrlParser: true, useCreateIndex: true }, error => {
   if (error) {
-    console.error(error);
-  }
-  else {
-    console.log('Connect with database established');
+    console.log(error);
+  } else {
+    console.log('Connected');
   }
 });
 
 process.on('SIGINT', () => {
-  mongoose.connection.close(function () {
+  mongoose.connection.close(() => {
     console.error('Mongoose default connection disconnected through app termination');
     process.exit(0);
   });
 });
 
-
-routes(app);
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, +'app/public/index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.info(`Server is running at ${config.port}`)
-});
+app.listen(process.env.PORT || 3000, () => console.log('Server is running'));
--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
 const config = {
-  databaseUrl: 'mongodb://tai:taitai1@ds147180.mlab.com:47180/tai',
-  port: 3000,
-  JwtSecret: "Secret"
+  port: process.env.PORT || 3000,
+  databaseUrl: process.env.MONGODB_URI || 'mongodb://tai:taitai1@ds147180.mlab.com:47180/tai',
+  JwtSecret: 'ajuwhqywekqwe12'
 }
 export default config
